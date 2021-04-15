b95be8c9975ef20bb69ee61d00b51b8854c62330
Nirman Narang <nirmannarang@gmail.com>
2021-04-15 15:51:38 +0530

    Create s390x_tests.sh

---
+++ s390x_tests.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+export SOURCE_ROOT=`pwd`
+sudo apt-get install -y wget tar make
+wget https://raw.githubusercontent.com/linux-on-ibm-z/scripts/master/Go/1.15/build_go.sh
+bash build_go.sh -y
+
+sudo apt-get install -y docker.io
+sleep 30s
+sudo nohup dockerd &
+sudo chmod ugo+rw /var/run/docker.sock
+
+docker ps
+
+export GOPATH=$SOURCE_ROOT/go
+mkdir -p $SOURCE_ROOT/go/src/github.com/moby
+cd $SOURCE_ROOT/go/src/github.com/moby
+git clone https://github.com/moby/moby
+cd moby
+git checkout tags/v20.10.6
+
+make test-unit
