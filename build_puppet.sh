a033aca85a93bde39b90080e8c3cbed5c282cc0f
Nirman Narang <nirmannarang@gmail.com>
2021-09-21 09:31:17 +0530

    Update build_puppet.sh

--- build_puppet.sh
+++ build_puppet.sh
@@ -311,10 +311,17 @@ if [[ "$USEAS" == "server" ]]; then
 		configureAndInstall |& tee -a "$LOG_FILE"
 		;;
 	
-	"rhel-8.1" | "rhel-8.2" | "rhel-8.3")
+	"rhel-8.2")
 		printf -- "Installing %s Server %s for %s \n" "$PACKAGE_NAME" "$SERVER_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
 		printf -- "Installing the dependencies for $PACKAGE_NAME from repository \n" |& tee -a "$LOG_FILE"
 		sudo yum install -y gcc-c++ tar unzip openssl-devel make git wget zip ant readline-devel gdbm-devel diffutils texinfo |& tee -a "$LOG_FILE"
+		configureAndInstall |& tee -a "$LOG_FILE"
+		;;
+		
+	"rhel-8.3" | "rhel-8.4")
+		printf -- "Installing %s Server %s for %s \n" "$PACKAGE_NAME" "$SERVER_VERSION" "$DISTRO" |& tee -a "$LOG_FILE"
+		printf -- "Installing the dependencies for $PACKAGE_NAME from repository \n" |& tee -a "$LOG_FILE"
+		sudo yum install -y gcc-c++ tar unzip openssl-devel make git wget zip ant readline-devel gdbm-devel diffutils |& tee -a "$LOG_FILE"
 		configureAndInstall |& tee -a "$LOG_FILE"
 		;;
 
